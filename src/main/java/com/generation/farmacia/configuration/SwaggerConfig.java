e9fec666f22978001bafdb7cb7244b879fdc3603
Eduardo Wada <156962925+eduardowada@users.noreply.github.com>
2024-07-22 17:55:08 -0300

    Subindo projeto finalizado

---
+++ src/main/java/com/generation/farmacia/configuration/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package com.generation.farmacia.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI springBlogPessoalOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Projeto CRUD Farmacia")
+                        .description("Projeto CRUD Farmacia - Generation Brasil")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("Eduardo Wada")
+                                .url("https://github.com/eduardowada"))
+                        .contact(new Contact()
+                                .name("Eduardo Wada")
+                                .url("https://github.com/eduardowada")
+                                .email("eduardo_wada@hotmail.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/eduardowada/CRUD-Farmacia"));
+    }
+
+    @Bean
+    OpenApiCustomizer customizerGlobalHeaderOpenApiCustomiser() {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations()
+                    .forEach(operation -> {
+                        ApiResponses apiResponses = operation.getResponses();
+
+                        apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+                        apiResponses.addApiResponse("201", createApiResponse("Objeto Pesistido!"));
+                        apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+                        apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+                        apiResponses.addApiResponse("401", createApiResponse("Acesso não Autorizado!"));
+                        apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+                        apiResponses.addApiResponse("404", createApiResponse("Objeto não Encontrado!"));
+                        apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+                    }));
+        };
+    }
+
+    private ApiResponse createApiResponse(String message) {
+        return new ApiResponse().description(message);
+    }
+}
---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O atributo descrição é obrigatório!")
+    private String descricao;
+
+    @NotNull(message = "O atributo preço é obrigatório!")
+    private Float preco;
+
+    @NotNull(message = "O atributo quantidade é obrigatório!")
+    private int quantidade;
+
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Categoria categoria;
+
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
